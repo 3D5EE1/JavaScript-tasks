3dfa549f6356432c1816d134291e97b4c582d2e1
Evgeny Gurin <rypylook@outlook.com>
2018-10-31 08:58:31 +0000

    drum script

--- my_samples.js
+++ my_samples.js
@@ -109,3 +109,62 @@ var m = new Proto("first prototype").print();
 
 var n = new Proto("second prototype");
 n.print();
+
+//---------------------------------------------------------------------------
+
+
+// Play drum by keyboard press
+window.addEventListener('click' , playDrum);
+function playDrum(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+};
+
+// Remove .playing class after transition action over
+const keys = document.querySelectorAll('.key');
+keys.forEach( key => key.addEventListener('transitionend' , removePlayingClass ) );
+function removePlayingClass(e)
+{
+    //if( e.propertyName != 'transform' ) return;
+    this.classList.remove('playing');
+}
+
+
+// Play music by mouse click
+keys.forEach( key => key.addEventListener('click' , playByMouse ) );
+function playByMouse(e)
+{
+    const keyCode = this.getAttribute('data-key');
+	const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+	audio.currentTime = 0;
+	audio.play();
+	const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+    key.classList.add('playing');
+}
+
+
+// ;(function () {
+
+// 	const keys = document.getElementsByClassName('key');
+// 	const audiolist = document.getElementsByTagName('audio');
+
+//   for (let key of keys) {
+//     key.onclick = function () {
+// 			key.style.transform = "scale(1.2)";
+// 			key.style.transition = "50ms ease-in";
+//       for ( audio of audiolist) {
+// 				if (audio.dataset.key === key.dataset.key) {
+// 					audio.play();
+// 					break;
+// 				}
+// 		}
+//     }
+//   }
+
+// })();
+
+
